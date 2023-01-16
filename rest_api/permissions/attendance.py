08c0c4e6611f7add6307ba3bf7668bd39c150fcd
Kenton <38125586+Kenton1989@users.noreply.github.com>
2023-01-17 00:30:00 +0800

    allow lab account modify through make up session

--- rest_api/permissions/attendance.py
+++ rest_api/permissions/attendance.py
@@ -1,6 +1,6 @@
 from django.db.models import QuerySet
 from django.utils import timezone
-from rest_api.models import User, StudentAttendance, TeacherAttendance, Session, Course
+from rest_api.models import User, StudentAttendance, TeacherAttendance, Session, Course, StudentMakeUpSession, Lab
 from rest_api.serializers import StudentAttendanceSerializer, TeacherAttendanceSerializer
 from .common import StaffManagedObjectPermission
 from rest_api.permissions.common import is_superuser, in_group
@@ -16,8 +16,9 @@ class BaseAttendanceAccessPermission(StaffManagedObjectPermission):
         assert isinstance(serializer,
                           (StudentAttendanceSerializer, TeacherAttendanceSerializer))
         session: Session = serializer['session']
+        attender: User = serializer['attender']
         return (
-            self._can_check_in(user, session, timezone.now()) or
+            self._can_check_in(user, attender, session, timezone.now()) or
             self._session_permission.has_update_object_permission(
                 user, request, view, session)
         )
@@ -25,8 +26,9 @@ class BaseAttendanceAccessPermission(StaffManagedObjectPermission):
     def has_update_object_permission(self, user, request, view, obj):
         assert isinstance(obj, (StudentAttendance, TeacherAttendance))
         session = obj.session
+        attender = obj.attender
         return (
-            self._can_check_in(user, session, timezone.now()) or
+            self._can_check_in(user, attender, session, timezone.now()) or
             super().has_update_object_permission(user, request, view, obj)
         )
 
@@ -49,11 +51,21 @@ class BaseAttendanceAccessPermission(StaffManagedObjectPermission):
 
         return course_queryset.none()
 
-    def _can_check_in(self, user: User, session: Session, request_time: datetime):
+    def _can_check_in(self, user: User, attender: User, session: Session, request_time: datetime):
+        if not Lab.objects.filter(user=user).exists():
+            return False
+
         return (
-            session.group.lab.user == user and
-            self._is_in_check_in_period(session, request_time)
+            (
+                session.group.lab.user == user and
+                self._is_in_check_in_period(session, request_time)
+            ) or
+            self._can_check_in_through_make_up(
+                user, attender, session, request_time)
         )
+
+    def _can_check_in_through_make_up(self, user: User, attender: User, original_session: Session, request_time: datetime) -> bool:
+        return False
 
     def _is_in_check_in_period(self, session: Session, request_time: datetime):
         return (
@@ -73,6 +85,22 @@ class BaseAttendanceAccessPermission(StaffManagedObjectPermission):
 
 
 class StudentAttendanceAccessPermission(BaseAttendanceAccessPermission):
+    def _can_check_in_through_make_up(self, user, attender, original_session, request_time):
+        make_up = StudentMakeUpSession.objects.filter(
+            original_session=original_session,
+            user=attender
+        ).first()
+
+        if not make_up:
+            return False
+
+        make_up_session = make_up.make_up_session
+
+        return (
+            make_up_session.group.lab.user == user and
+            self._is_in_check_in_period(make_up_session, request_time)
+        )
+
     def get_managed_data(self, user: User, object_queryset: QuerySet = StudentAttendance.objects.all()) -> QuerySet:
         return super().get_managed_data(user, object_queryset)
 
