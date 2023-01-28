da57a4f6e42acf68067b2449ee7f73228372837e
Kenton <38125586+Kenton1989@users.noreply.github.com>
2023-01-29 00:01:52 +0800

    add missing is_active filter

--- rest_api/views/user.py
+++ rest_api/views/user.py
@@ -3,14 +3,22 @@ from .base import BaseModelViewSet
 from rest_api.serializers import UserSerializer
 from rest_api.models import User
 from rest_api.permissions import UserAccessPermission
+from django_filters import rest_framework as filters
+
+
+class UserFilterSet(filters.FilterSet):
+    class Meta:
+        model = User
+        fields = ('is_active',)
 
 
 class UserViewSet(BaseModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    filter_backends = (SearchFilter,)
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter,)
     search_fields = ('username', 'display_name',)
+    filterset_class = UserFilterSet
 
     permission_classes = (UserAccessPermission, )
 
